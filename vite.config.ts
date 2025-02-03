f397e7a41978774b9c4cb951fe5b75fd72e228a4
GMO <78043718+gurkanozil@users.noreply.github.com>
2025-02-03 19:20:13 +0100

    UPDATE CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ranakaya/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
-
-export default defineConfig({
-  base: '/ranakaya/',
-  plugins: [react()],
 });
