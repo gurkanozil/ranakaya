f249f5c99a5f80c19934ad1d70fbc8ce8c6a5874
gurkanozil <gurkanozil@live.nl>
2025-02-07 23:51:04 +0100

    Refine Sidebar styling and responsiveness with minor layout adjustments

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,15 +19,15 @@ const Sidebar: React.FC<SidebarProps> = ({
   navItems,
 }) => {
   return (
-    <aside className="flex left-0 top-0 md:h-dvw w-1/3 bg-slate-300 p-12 flex flex-col">
+    <aside className="flex left-0 top-0 md:h-dvw md:w-1/3 bg-slate-300 p-12 flex flex-col">
       <div className="flex flex-col items-center mb-12">
         <img
           src={profileImage}
           alt="Profile"
-          className="w-48 h-48 rounded-full object-cover border-1 border-pink-500 shadow-2xl mb-6 duration-300 ease-in hover:scale-125 hover:mb-10 hover:mt-6"
+          className="w-48 h-48 rounded-full object-cover shadow-2xl mb-6 duration-200 ease-in hover:scale-125 hover:mb-10 hover:mt-6"
         />
         <h1 className="text-4xl font-extrabold text-pink-500">RANA KAYA</h1>
-        <p className="text-xl font-thin text-[16px] text-gray-600 tracking-widest">DIGITAL DESIGNER</p>
+        <p className="text-xl font-thin text-gray-600 tracking-widest">DIGITAL DESIGNER</p>
       </div>
 
       <nav className="flex-1 -mt-10 flex text-center justify-center">
@@ -48,19 +48,19 @@ const Sidebar: React.FC<SidebarProps> = ({
       </nav>
 
       <div className="flex justify-center space-x-4 pt-6 border-t border-blue-50">
-        <a href="mailto:ranakaya@outlook.be" // Place  email
+        <a href="mailto:ranakaya@outlook.be"
           className="text-gray-600 hover:text-blue-700 transition-colors"
         ><Mail size={32} /></a>
-        <a href="tel:+32 474 38 64 24" // Place phone number
+        <a href="tel:+32 474 38 64 24"
           className="text-gray-600 hover:text-blue-700 transition-colors"
         ><Phone size={32} /></a>
         <a href="https://www.facebook.com/yuna.shin.3975"
           className="text-gray-600 hover:text-blue-700 transition-colors"
         ><Facebook size={32} /></a>
-        <a href="https://www.instagram.com/kayaaranaa/" // Place Instagram
+        <a href="https://www.instagram.com/kayaaranaa/"
           className="text-gray-600 hover:text-blue-700 transition-colors"
         ><Instagram size={32} /></a>
-        <a href="https://www.linkedin.com/in/rana-kaya-64aaa5333/" // Place Facebook
+        <a href="https://www.linkedin.com/in/rana-kaya-64aaa5333/"
           className="text-gray-600 hover:text-blue-700 transition-colors"
         ><Linkedin size={32} /></a>
       </div>
