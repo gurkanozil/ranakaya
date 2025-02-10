11a510c505338a99f5b6579527e78197509d1423
GMO <78043718+gurkanozil@users.noreply.github.com>
2025-02-10 18:35:26 +0100

    testing main.tsx render on all devices

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,11 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  window.innerWidth >= 768 ? (
+  //window.innerWidth >= 768 ? (
     <StrictMode>
       <App />
     </StrictMode>
-  ) : ( // if on mobile
-    <div className="flex left-0 top-0 h-screen w-full bg-slate-300 p-6 pt-48 text-center justify-center">This website is best viewed on desktop devices</div>
-  )
+  //) : ( // if on mobile
+  //  <div className="flex left-0 top-0 h-screen w-full bg-slate-300 p-6 pt-48 text-center justify-center">This website is best viewed on desktop devices</div>
+  //)
 );
